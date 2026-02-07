7203178ca5e9ea01040d9468551a210532332b92
Raouf1812 <roofraafet@gmail.com>
2026-02-07 04:00:29 +0200

    Add files via upload

---
+++ الشيتات.js
@@ -0,0 +1,14 @@
+// ملف البيانات - data.js
+const subjects = [
+    { key: 'plc', label: 'PLC (متحكمات)', eng: 'محمد الجيار' },
+    { key: 'صيانة', label: 'صيانة أجهزة', eng: ' أسراء' },
+    { key: 'اتصالات', label: 'إتصالات', eng: ' معرفش' },
+    { key: 'KAB', label: 'KAB (العمل الحر)', eng: ' شيرين' },
+    { key: 'معامل', label: 'معامل تطبيقية', eng: ' معرفش' },
+    { key: 'تقارير', label: 'تقارير فنية', eng: ' معرفش' },
+    { key: 'دوائر', label: 'دوائر متقدمة', eng: ' سوسن' }
+];
+
+const allData = [
+    { name: "شيت رقم 1", date: "2999-09-01", sub: "plc", url: "RR/pngegg.png" },
+];
