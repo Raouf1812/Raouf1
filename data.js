31da7b3099c1316ced9177f0e9480097def5cb2a
Raouf1812 <roofraafet@gmail.com>
2026-02-09 12:54:19 +0200

    Add new entries to data.js

--- data.js
+++ data.js
@@ -664,7 +664,25 @@ var libraryData = {
                 "name": "مذكرة اتصالات معهد الزقازيق",
                 "link": "https://drive.google.com/uc?export=download&id=1Tp3pO42dgwxF8mwhvGvbN4OqNQnjg96Q",
                 "size": "47.4 MB"
-            }
+            }, 
+            {
+    "id": "1770634367837",
+    "name": "اتصالات م سمر صبحي",
+    "link": "https://drive.google.com/uc?export=download&id=1FJZ5C3HNoEvV4qvlHoKGAce-uBUvEmPD",
+    "size": "5.5 MB"
+},
+            {
+    "id": "1770634324994",
+    "name": "اتصالات المهندسه اسماء",
+    "link": "https://drive.google.com/uc?export=download&id=1DWOpkVIMW0eTCERL8A721bkD8ca3pqhb",
+    "size": "15.3 MB"
+},
+            {
+    "id": "1770634253844",
+    "name": "مادة اتصالات Abdelmonem",
+    "link": "https://drive.google.com/uc?export=download&id=1g8vFrN0NwxXGhhzAX6PiMx6Y7u1GBZ-H",
+    "size": "25.2 MB"
+},
         ],
         "books": [
             {
