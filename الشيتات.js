33fb60da8cf7f61817bf2ade9ded60b9e500bbfa
Raouf1812 <roofraafet@gmail.com>
2026-02-11 19:39:49 +0200

    Fix duplicate entry in الشيتات.js
    Removed duplicate entry from الشيتات.js.

--- الشيتات.js
+++ الشيتات.js
@@ -49,10 +49,4 @@ const allData = [
         "sub": "اتصالات",
         "url": "https://i.ibb.co/GZ3M96q/1000292085.jpg"
     }
-];{
-        "name": "شيت رقم 1",
-        "date": "2026-02-10",
-        "sub": "اتصالات",
-        "url": "https://i.ibb.co/GZ3M96q/1000292085.jpg"
-    }
 ];
